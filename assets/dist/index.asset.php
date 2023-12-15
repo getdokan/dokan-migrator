8f5ea8c84085b74e46141e98dd1bcc711187f87d
Nurul Umbhiya <zikubd@gmail.com>
2023-12-15 10:55:07 +0600

    chore: bump version to 1.1.1

--- assets/dist/index.asset.php
+++ assets/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => '5d6edc644b8a51eb7bb7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-element', 'wp-i18n'), 'version' => 'bc4702a59b266b87639a');
